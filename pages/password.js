895ff8ac9ea9ab0bfe8589308dc3d257f72a3225
Drkeck <k3ckl3y.a@gmail.com>
2021-12-09 15:57:00 -0800

    added a css module to help with styling the password page specifically

--- pages/password.js
+++ pages/password.js
@@ -1,4 +1,5 @@
 import {useState} from "react"
+import styles from "./password.module.css"
 
 function PassWall() {
     const [input, setInput] = useState("");
@@ -8,9 +9,9 @@ function PassWall() {
     }
 
     return(
-        <div className="background" style={{background: "gray"}}>
-            <form onSubmit={handlePassword}>
-                <label htmlFor="pass">password:</label>
+        <div className={styles.background}>
+            <form onSubmit={handlePassword} className={styles.forum}>
+                <label htmlFor="pass" >password:</label>
                 <input value={input} onChange={(e) => setInput(e.target.value)} id="pass" type="password"/>
             </form>
         </div>
