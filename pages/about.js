cec458755e6466af58c3d0c7278a4d6da6ee609e
Drkeck <k3ckl3y.a@gmail.com>
2021-12-08 17:53:40 -0800

    added a new page as i learn more about nextjs, so far i am loving the little bit that i did in it

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+function About() {
+
+    return(
+        <div>
+            <h1>Hello there</h1>
+            <p>This is my first time ever using next.js and i was curious to see how it works in action.</p>
+            <p>honestly how this whole thing works is a little weird but at the same time its also way more straight forward than base React and since this is also a server implimentation you are able to use specific things here like being able to build api end points that will link into databases</p>
+            <p>and all of this is just lightning fast</p>
+        </div>
+    )
+}
+
+export default About
