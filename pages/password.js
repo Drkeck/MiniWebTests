a1fd735c87b6c0cb2b02046c4c203521fadec92f
Drkeck <k3ckl3y.a@gmail.com>
2021-12-10 15:09:51 -0800

    been trying to make a cool little log in section for my site

--- pages/password.js
+++ pages/password.js
@@ -10,10 +10,21 @@ function PassWall() {
 
     return(
         <div className={styles.background}>
-            <form onSubmit={handlePassword} className={styles.forum}>
-                <label htmlFor="pass" >password:</label>
-                <input value={input} onChange={(e) => setInput(e.target.value)} id="pass" type="password"/>
-            </form>
+
+            <div className={styles.forum} >
+                <h3 className={styles.marker}>password:</h3>
+                <form onSubmit={handlePassword}>
+                    <input value={input} onChange={(e) => setInput(e.target.value)} id="pass" type="password"/>
+                </form>
+            </div>
+            <div className={styles.stencil}>
+                <div className={styles.line1} />
+                <div className={styles.line2} />
+                <div className={styles.line3} />
+                <div className={styles.line4} />
+                <div className={styles.line5} />
+                <div className={styles.line6} />
+            </div>
         </div>
     )
 }
